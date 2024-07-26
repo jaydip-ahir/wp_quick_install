6c89549b32d33b85db1f8194e609fe083f76a34d
Jaydip Ahir <159106695+jaydip-ahir@users.noreply.github.com>
2024-07-26 17:39:04 +0530

    Update install_wp_quick_install.sh

--- install_wp_quick_install.sh
+++ install_wp_quick_install.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
-sudo snap remove curl
-sudo apt install curl
+
+# Ensure curl is installed
+if ! command -v curl &> /dev/null; then
+    sudo apt-get update
+    sudo apt-get install -y curl
+fi
+
+# Download the wp_quick_install.sh script and save it to /usr/local/bin/wp_quick_install
+sudo curl -o /usr/local/bin/wp_quick_install https://raw.githubusercontent.com/jaydip-ahir/wp_quick_install/master/wpinstall.sh
+
 # Make the script executable
-chmod +x /usr/local/bin/
-
-# Download the wp_quick_install.sh script
-curl -o /usr/local/bin/ https://raw.githubusercontent.com/jaydip-ahir/wp_quick_install/master/wpinstall.sh
-
+sudo chmod +x /usr/local/bin/wp_quick_install
 
 # Notify the user
 echo "wp_quick_install has been installed. You can now run it by typing 'wp_quick_install' in your terminal."
