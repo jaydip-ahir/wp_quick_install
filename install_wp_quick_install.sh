898c1b76977df213659b85b9817e76ff038ce031
jaydip-ahir <jaydip@krishaweb.com>
2024-07-26 16:37:24 +0530

    Change file path

--- install_wp_quick_install.sh
+++ install_wp_quick_install.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Download the wp_quick_install.sh script
-curl -o /usr/local/bin/wp_quick_install https://raw.githubusercontent.com/jaydip-ahir/wp_quick_install/master/wpinstall.sh
-
 # Make the script executable
-chmod +x /usr/local/bin/wp_quick_install
+chmod +x /usr/local/bin/
+
+# Download the wp_quick_install.sh script
+curl -o /usr/local/bin/ https://raw.githubusercontent.com/jaydip-ahir/wp_quick_install/master/wpinstall.sh
+
 
 # Notify the user
 echo "wp_quick_install has been installed. You can now run it by typing 'wp_quick_install' in your terminal."
